9ea367d559e38c352e43cc9bc26d2c680dbf8c7e
ADITYA KAR <AKAR016012@ICLOUD.COM>
2021-11-03 03:30:11 -0500

    initial commit

---
+++ public/controller/routes.js
@@ -0,0 +1,9 @@
+import * as HomePage from "../viewpage/home_page.js";
+
+//user pages routes
+import * as UserPage from "../viewpage/user_page.js";
+import * as ReviewPage from "../viewpage/reviews_page.js";
+import * as UserProfilePage from "../viewpage/user_profile_page.js";
+import * as UserPurchasePage from "../viewpage/user_purchases_page.js";
+import * as UserReviewPage from "../viewpage/user_reviews_page.js";
+import * as UserShoppingCartPage from "../viewpage/user_shopping_cart_page.js";
---
+++ public/viewpage/element.js
@@ -0,0 +1,75 @@
+//menu elements.........
+
+export const menuHome = document.getElementById("menu-home");
+export const menuSignOut = document.getElementById("menu-signout");
+export const menuProfile = document.getElementById("menu-profile");
+export const menuShoppingCart = document.getElementById(
+  "menu-button-shoppingcart"
+);
+
+//users buttons
+export const menuUserPurchases = document.getElementById("menu-user-purchases");
+export const menuUserProfucts = document.getElementById("menu-user-products");
+export const menuUserProductReview = document.getElementById(
+  "menu-user-product-reviews"
+);
+export const formSearch = document.getElementById("form-search");
+
+//admin buttons
+export const menuAdminPurchases = document.getElementById(
+  "menu-admin-purchases"
+);
+export const menuAdminProducts = document.getElementById("menu-admin-products");
+export const menuAdminUsers = document.getElementById("menu-admin-users");
+export const reviewCheck = document.getElementById("menu-Review-check-admin");
+
+//sign up
+export const formSignup = document.getElementById("form-signup");
+export const formSignUpPassowrdError = document.getElementById(
+  "form-signup-password-error"
+);
+export const menuSignup = document.getElementById("button-form-signup");
+
+//sign in
+export const formSignIn = document.getElementById("form-signin");
+
+//Add new Products
+export const formAddProduct = document.getElementById("form-add-product");
+export const formAddProductError = {
+  name: document.getElementById("form-add-product-error-name"),
+  price: document.getElementById("form-add-product-error-price"),
+  summary: document.getElementById("form-add-product-error-summary"),
+  image: document.getElementById("form-add-product-error-image"),
+};
+export const imageTagProduct = document.getElementById(
+  "form-add-product-image-tag"
+);
+export const formImageAddButton = document.getElementById(
+  "form-add-product-image-button"
+);
+
+//edit products
+export const formEditProduct = document.getElementById("form-edit-product");
+export const formEditImageTag = document.getElementById(
+  "form-edit-product-image-tag"
+);
+export const formEditProductImageButton = document.getElementById(
+  "form-edit-product-image-tag"
+);
+export const formEditProductError = {
+  name: document.getElementById("form-edit-product-error-name"),
+  price: document.getElementById("form-edit-product-error-price"),
+  summary: document.getElementById("form-edit-product-error-summary"),
+};
+
+//popup infobox
+export const popupInfoTilte = document.getElementById("modal-info-title");
+export const popupInfoBody = document.getElementById("modal-info-body");
+
+//transaction view
+export const modalTransactionTitle = document.getElementById(
+  "modal-transaction-title"
+);
+export const modalTransactionBody = document.getElementById(
+  "modal-transaction-body"
+);
